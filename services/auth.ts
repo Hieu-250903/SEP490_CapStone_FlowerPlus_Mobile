52013a17aa496a02d166b7fa38de3bdef8510c19
Nguyễn Minh Hiếu <124897137+Hieu-250903@users.noreply.github.com>
2025-12-15 18:45:47 +0700

    Update auth.ts

--- services/auth.ts
+++ services/auth.ts
@@ -43,7 +43,7 @@ export const userLoginApi = async (credentials: {
       username: credentials.email,
       password: credentials.password,
     });
-    return response;
+    return response.data;
   } catch (error) {
     throw error;
   }
